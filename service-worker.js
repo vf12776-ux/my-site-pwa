0f031e8a760c4102afe1e28df9fb39326076a017
none <https://github.com/vf12776-ux>
2025-12-28 23:38:53 +0300

    FIX: Правильный публичный адрес croissant77.ru/page2
    - Заменен адрес редактора Craftum на публичный croissant77.ru/page2
    - Обновлены названия для Croissant77
    - Упрощен service-worker
    - Теперь PWA безопасен и ведет на публичную версию сайта

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,18 @@
-// Простой PWA регистратор
-self.addEventListener("install", e => self.skipWaiting());
-self.addEventListener("activate", e => self.clients.claim());
+// Простой Service Worker для PWA
+const CACHE_NAME = 'croissant77-pwa-v1';
+
+self.addEventListener('install', event => {
+    console.log('[PWA Croissant77] Установлен');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    console.log('[PWA Croissant77] Активирован');
+    event.waitUntil(self.clients.claim());
+});
+
+// Для PWA не кешируем, только редирект
+self.addEventListener('fetch', event => {
+    // Можно добавить кеширование статики позже
+    return;
+});
